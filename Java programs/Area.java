da0a138aa7d08222fd7662a89facf386f5106766
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-04 00:30:55 +0530

    Added new file

---
+++ Java programs/Area.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Area
+{
+    public static void main(String args[])
+    {
+        int choice,r,s,l,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Area");
+        System.out.println("1.Circle");
+        System.out.println("2.Square");
+        System.out.println("3.Rectangle");
+        System.out.print("Enter your choice: ");
+        choice=sc.nextInt();
+        switch(choice)
+        {
+            case 1:System.out.print("Enter the radius:");
+                   r=sc.nextInt();
+                   System.out.println("The area of the circle is:"+(3.14*r*r));
+                   break;
+            case 2:System.out.print("Enter the side:");
+                   s=sc.nextInt();
+                   System.out.println("The area of the square is:"+(s*s));
+                   break;
+            case 3:System.out.print("Enter the length:");
+                   l=sc.nextInt();
+                   System.out.print("Enter the breadth:");
+                   b=sc.nextInt();
+                   System.out.println("The area of the rectangle is:"+(l*b));
+                   break;
+            default:System.out.println("Invalid");       
+                   
+        }  
+   }
+}
