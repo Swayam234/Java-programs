98277fa2a80ddc6ddaf6b10f3c7ab2d865db485a
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-06 18:27:01 +0530

    Added file1

---
+++ Java programs/forLoop1.java
@@ -0,0 +1,11 @@
+class forLoop1
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=5;i++)
+        {
+            System.out.println("Hello World!!!");
+        }
+    }
+}
