88817417fc681259ac45c10a4f639b73c7c9cca2
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-06 19:13:46 +0530

    Added file3

---
+++ Java programs/forLoop3.java
@@ -0,0 +1,14 @@
+class forLoop3
+{
+    public static void main(String args[])
+    {
+        int i,sum=0;
+        for(i=1;i<=5;i++)
+        {
+            System.out.println(i);
+            sum=sum+i;
+        }
+        System.out.println("sum="+sum);
+       
+    }
+}
