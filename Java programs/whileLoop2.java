f8859c6bd2974eb8fd243bd35da2052a46486fea
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-07 17:12:34 +0530

    Added file2

---
+++ Java programs/whileLoop2.java
@@ -0,0 +1,14 @@
+class whileLoop2
+{
+    public static void main(String args[])
+    {
+        int i=1,sum=0;
+        while(i<=5)
+        {
+            System.out.println(i);
+            sum=sum+i;
+            i++;
+        }
+        System.out.println("The sum is:"+sum);
+    }
+}
