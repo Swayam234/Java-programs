89cf465f853a5d75190b54ef6ed227fb318ad7de
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-13 13:42:53 +0530

    Add imp7 file

---
+++ Java programs/PerfectNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class PerfectNumber
+{
+    public static void main(String args[])
+    {
+        int i,num,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        num=sc.nextInt();
+        for(i=1;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                System.out.println(i);
+                sum=sum+i;
+            }
+        }
+        System.out.println("The sum is:"+sum);
+        if(num==sum)
+        {
+            System.out.println("It is a perfect number");
+        }
+        else
+        {
+            System.out.println("It is not a perfect number");
+        }
+        
+    }
+}
