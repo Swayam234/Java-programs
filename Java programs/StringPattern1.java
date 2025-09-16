9724916f3c0444f2480eba4dc49baff8b77605ca
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-16 12:16:41 +0530

    Add imp16 file

---
+++ Java programs/StringPattern1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class StringPattern1
+{
+    public static void main(String args[])
+    {
+        int i,l;
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string:");  
+        s=sc.next();
+        l=s.length();  
+        for(i=0;i<l;i++)
+        {
+            System.out.println(s.charAt(i));
+        }
+        
+    }
+}
