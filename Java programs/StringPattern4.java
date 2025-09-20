f3bd36d7fbaf7835bfa109a972711c26e4bc8f4f
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-20 18:28:53 +0530

    Add imp19 file

---
+++ Java programs/StringPattern4.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class StringPattern4
+{
+    public static void main(String args[])
+    {
+        int i,j,l,ch;
+        String s = "BLUEJ";
+        Scanner sc = new Scanner(System.in);
+        do
+        {
+         System.out.println("Pattern program");
+         System.out.println("1.Triangle Pattern");
+         System.out.println("2.Inverted Triangle Pattern");
+         System.out.print("Enter a choice:");
+         ch=sc.nextInt();
+         switch(ch)
+         {
+            case 1:
+            l=s.length(); 
+            for(i=0;i<l;i++) 
+            {
+                for(j=0;j<=i;j++)
+                {
+                    System.out.print(s.charAt(j));
+                }
+                System.out.println();
+            }
+            break;
+            case 2:
+            l=s.length();
+            for(i=l;i>=0;i--)
+            {
+                for(j=0;j<i;j++)
+                {
+                    System.out.print(s.charAt(j));
+                }
+                System.out.println();
+            }
+            break;
+         }
+    }while(ch<=2);     
+  }
+}
