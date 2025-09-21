2c198cfdef589a9caac78fdf5e0fa68743fd45bc
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-21 22:42:49 +0530

    Add imp22 file

---
+++ Java programs/Function3.java
@@ -0,0 +1,12 @@
+class Function3
+{
+    public static void main(String args[])
+    {
+        Function3 f2 =  new Function3();
+        f2.amount();
+    }
+    void amount()
+    {
+        System.out.println("The amount is: "+(5*12));
+    }
+}
