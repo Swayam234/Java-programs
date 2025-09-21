da283bea48ca3e3bbc21089fc221c38c9b96fc49
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-21 22:41:25 +0530

    Add imp20 file

---
+++ Java programs/Function1.java
@@ -0,0 +1,12 @@
+class Function1
+{
+    public static void main(String args[])
+    {
+        Function1 f = new Function1();
+        f.sum();
+    }
+    void sum()
+    {
+        System.out.println("The sum is: "+(10+20));
+    }
+}
