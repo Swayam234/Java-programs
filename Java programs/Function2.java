5cbb9e4bd04de04b2a7dadd08b4392e28c697544
Swayam234 <swayam.17044@sakec.ac.in>
2025-09-21 22:42:18 +0530

    Add imp21 file

---
+++ Java programs/Function2.java
@@ -0,0 +1,12 @@
+class Function2
+{
+    public static void main(String args[])
+    {
+        Function2 f1 = new Function2();
+        f1.area();
+    }
+    void area()
+    {    
+        System.out.println("The area of the circle is: "+(3.14*4*4));
+    }
+}
