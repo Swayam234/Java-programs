64817cbac3eb561555c01a5e2aafdfd2d284494e
Swayam234 <swayam.17044@sakec.ac.in>
2025-10-03 18:40:43 +0530

    Add imp36 file

---
+++ Java programs/Alpha4.java
@@ -0,0 +1,16 @@
+class Alpha4
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Printing all uppercase letters:");
+        for (char c = 'A'; c <= 'Z'; c++) 
+        {
+            System.out.print(c + " ");
+        }
+        System.out.println("\n\nPrinting digits 0 to 9:");
+        for (char d = '0'; d <= '9'; d++) 
+        {
+            System.out.print(d + " ");
+        }
+    }
+}
